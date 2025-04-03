1d6cdab250134e2f67db3ce2c730ed3e9a59dceb
Filahan <filah.anas@gmail.com>
2025-04-03 22:19:28 +0200

    Remove dark theme color from layout metadata and refactor BlogPage component to use props for article retrieval, enhancing code clarity.

--- app/blog/[id]/page.tsx
+++ app/blog/[id]/page.tsx
@@ -2,10 +2,13 @@ import { notFound } from 'next/navigation';
 import BlogArticle from '@/components/BlogArticle';
 import blogData from '@/data/blog.json';
 
+type Props = {
+  params: { id: string }
+  searchParams: { [key: string]: string | string[] | undefined }
+}
 
-
-export default async function BlogPage({ params }: { params: { id: string } }) {
-  const article = blogData.articles.find((article) => article.id === params.id);
+export default async function BlogPage(props: Props) {
+  const article = blogData.articles.find((article) => article.id === props.params.id);
 
   if (!article) {
     notFound();
