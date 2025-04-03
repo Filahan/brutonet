b8d6b2d066583c934a2c591cac9110bb238dc4d5
Filahan <filah.anas@gmail.com>
2025-04-03 22:00:50 +0200

    Refactor BlogPage component to remove searchParams from props, streamlining the article retrieval process.

--- app/blog/[id]/page.tsx
+++ app/blog/[id]/page.tsx
@@ -4,10 +4,9 @@ import blogData from '@/data/blog.json';
 
 type BlogPageProps = {
   params: { id: string };
-  searchParams: { [key: string]: string | string[] | undefined };
 }
 
-export default async function BlogPage({ params, searchParams }: BlogPageProps) {
+export default async function BlogPage({ params }: BlogPageProps) {
   const article = blogData.articles.find((article) => article.id === params.id);
 
   if (!article) {
