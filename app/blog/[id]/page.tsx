e38497ab6c3c76a49fea812eb5ffae5f0325851c
Filahan <filah.anas@gmail.com>
2025-04-03 22:09:31 +0200

    Refactor BlogPage component to simplify props by removing searchParams, enhancing code clarity.

--- app/blog/[id]/page.tsx
+++ app/blog/[id]/page.tsx
@@ -2,12 +2,9 @@ import { notFound } from 'next/navigation';
 import BlogArticle from '@/components/BlogArticle';
 import blogData from '@/data/blog.json';
 
-type BlogPageProps = {
-  params: { id: string };
-  searchParams?: { [key: string]: string | string[] | undefined };
-}
 
-export default async function BlogPage({ params }: BlogPageProps) {
+
+export default async function BlogPage({ params }: { params: { id: string } }) {
   const article = blogData.articles.find((article) => article.id === params.id);
 
   if (!article) {
